27c4bbf8f7414282feb0f58cfdbf622da5443cd5
huangjx <trustfor@sina.cn>
2018-08-27 12:40:47 +0800

    字体图标引入

--- mi/src/main.js
+++ mi/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'normalize.css'
+import 'normalize.css'  //样式处理
+import './assets/css/reset.css' //样式重置
+import './assets/fonts/iconfont.css' //字体图标
+
+
 Vue.config.productionTip = false
 
 new Vue({
