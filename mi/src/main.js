732a3cbf1ca5600a8c9bd3ea4ba01754775758e1
huangjx <trustfor@sina.cn>
2018-08-27 12:29:22 +0800

    normalize.css引入

--- mi/src/main.js
+++ mi/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import 'normalize.css'
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
