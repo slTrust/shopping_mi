50cd2cb6bb1d7050da6a3fc6b4cb5fd9cba1eadb
huangjx <trustfor@sina.cn>
2018-08-29 14:38:53 +0800

    接口数据的渲染,下步将内容区域重复内容抽离成组件

--- mi/src/views/MiCategory.vue
+++ mi/src/views/MiCategory.vue
@@ -17,146 +17,37 @@
           </ul>
         </div>
         <div class="list-wrap">
-          <div class="list-item category0">
+          <div
+            v-for="(list,index) in categoryList"
+            :key="list.category_id"
+            :class="'category'+index"
+            class="list-item">
             <div class="component-list-main">
-              <div class="cells_auto_fill" index="0">
+              <div class="cells_auto_fill">
                 <a class="exposure items">
-                  <img src="//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/711275806886438be20e33ec943a904a.jpg?thumb=1&amp;w=500&amp;h=200" lazy="loaded">
+                  <img :src="list.category_img" lazy="loaded">
                 </a>
               </div>
-              <div name="m1" class="category_title">
-                <span>手机</span>
-              </div>
-              <div class="category_group box-flex" index="2">
-                <div class="box">
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/cae1e9af28aebb1d0e065cbdc08d8f66!120x120.png" lazy="loaded">
-                      </div>
-                      <div class="name">小米8</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/dc6165304dcced2a3b2cc574e4af9b4a!120x120.png" lazy="loaded">
-                      </div>
-                      <div class="name">小米8 SE</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/06cabb01bbe4ac5c819ee101e4446a16!120x120.png" lazy="loaded">
-                      </div>
-                      <div class="name">小米MIX 2S</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/745cd6cbffce6a1c1c1cafbd5abbfed0!120x120.png" lazy="loaded">
-                      </div>
-                      <div class="name">黑鲨游戏手机</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item" data-stat-id="dd150121ab5f594b">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/74049580f582c72efdde7c80fbdbd223!120x120.jpg" lazy="loaded">
-                      </div>
-                      <div class="name">红米S2</div>
-                    </a>
+              <template v-for="item in list.category_list">
+                <div class="category_title">
+                  <span>{{item.category_title}}</span>
+                </div>
+                <div class="category_group box-flex">
+                  <div class="box">
+                    <div
+                      v-for="product in item.category_group"
+                      :key="product.category_id"
+                      class="product">
+                      <a class="exposure item">
+                        <div class="img">
+                          <img class="big" :src="product.img_url" lazy="loaded">
+                        </div>
+                        <div class="name">{{product.category_name}}</div>
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div name="m3" class="category_title">
-                <span>电视</span>
-              </div>
-              <div class="category_group box-flex" index="4">
-                <div class="box">
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/bf2dc71cbf6fa8413b7ade09719155a0!120x120.jpg" lazy="loaded">
-                      </div>
-                      <div class="name">75英寸 电视4</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/07037fbe905f42477d4816ee64d857b5!120x120.jpg" lazy="loaded">
-                      </div>
-                      <div class="name">55"4S 曲面</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/4129babe8bee3dd1c9ddcbb112b319f5!120x120.jpg" lazy="loaded">
-                      </div>
-                      <div class="name">55英寸 4X</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/fa50f303fa906aa00af309fa5b10dda8!120x120.jpg" lazy="loaded">
-                      </div>
-                      <div class="name">32英寸 4C</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/febe6b5c777b1d91e08d44b62d71092d!120x120.jpg" lazy="loaded">
-                      </div>
-                      <div class="name">43英寸 4S</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/48437e4f9e6d65d5adfdd51add78b59b!120x120.jpg" lazy="loaded">
-                      </div>
-                      <div class="name">43英寸 青春版</div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div name="m5" class="category_title">
-                <span>电脑</span>
-              </div>
-              <div class="category_group box-flex" index="6">
-                <div class="box">
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/ccad3ac9a43a21a606a392cf6a14b091!120x120.jpg" lazy="loaded">
-                      </div>
-                      <div class="name">15.6"游戏本</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a  class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/dce7af666be089a9a69d23559c0d180c!120x120.png" lazy="loaded">
-                      </div>
-                      <div class="name">13.3"增强版</div>
-                    </a>
-                  </div>
-                  <div class="product">
-                    <a class="exposure item">
-                      <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/5c14cc0183352b8d6f4751a84016756c!120x120.png" lazy="loaded">
-                      </div>
-                      <div class="name">12.5"office版</div>
-                    </a>
-                  </div>
-                </div>
-              </div>
+              </template>
             </div>
           </div>
         </div>
