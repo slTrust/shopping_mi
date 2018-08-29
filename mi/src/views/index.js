8015bca4276b7ed596a0aba28f44624edfc5fabd
huangjx <trustfor@sina.cn>
2018-08-29 14:19:36 +0800

    分类页面初始化 路由源由tmp切换到views

--- mi/src/views/index.js
+++ mi/src/views/index.js
@@ -1,22 +1,49 @@
-import MiLogin from './MiLogin.vue'
-import MiHome from './MiHome.vue'
-
-// 路由的优化
+const MiLogin = () => import('./MiLogin.vue')
+const MiHome = () => import('./MiHome.vue')
+const MiCategory = () => import('./MiCategory.vue')
+const MiCart = () => import('./MiCart.vue')
+const MiUser = () => import('./MiUser.vue')
 
 export default [
   {
-    path: '/home',
-    name: 'home',
-    component: MiHome
+    path: '/',
+    redirect: 'home'
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('./About.vue')
+    path: '/home',
+    name: 'home',
+    component: MiHome,
+    meta: {
+      index: 0
+    }
   },
   {
     path: '/login',
     name: 'login',
     component: MiLogin
+  },
+  {
+    path: '/category',
+    name: 'category',
+    component: MiCategory,
+    meta: {
+      index: 1
+    }
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: MiCart,
+    meta: {
+      index: 2
+    }
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: MiUser,
+    meta: {
+      index: 3
+    }
   }
 ]
